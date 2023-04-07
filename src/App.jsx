da32fb11323ec3fdec548239b8a33470355b0f6e
zang3tsu88 <maximenya.a@gmail.com>
2023-04-07 16:14:24 +0300

    Ported styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <header class="header">
+        <img
+          class="header__logo"
+          src="<%=require('./images/logo-mesto_color_white.svg')%>"
+          alt="логотип
+        место россия"
+        />
+      </header>
+      <main class="main">
+        <section class="profile">
+          <button class="profile__avatar-btn">
+            <img
+              class="profile__avatar"
+              src="<%=require('./images/avatar_jack_iv_kusto.jpg')%>"
+              alt="Аватарка пользователя"
+            />
+          </button>
+          <div class="profile__user-info">
+            <h1 class="profile__user-name">Жак-Ив Кусто</h1>
+            <button
+              class="profile__edit-btn"
+              type="button"
+              aria-label="Редактировать профиль"
+            ></button>
+            <p class="profile__user-occupation">Исследователь океана</p>
+          </div>
+          <button
+            class="profile__btn-add-img"
+            type="button"
+            aria-label="Добавить изображение"
+          ></button>
+        </section>
+        <section class="cards">
+          <ul class="cards__list">
+          </ul>
+        </section>
+      </main>
+      <footer class="footer">
+        <p class="footer__copyright" lang="en">&copy; 2022. Mesto Russia</p>
+      </footer>
+    </>
+
   )
 }
 
