69499bfb3b40968a36e09836fffa5abe66d6d9b7
zang3tsu88 <maximenya.a@gmail.com>
2023-06-24 12:45:35 +0300

    fix: image regex

--- src/components/AddPlacePopup.jsx
+++ src/components/AddPlacePopup.jsx
@@ -71,7 +71,7 @@ function AddPlacePopup({ isOpen, onClose, onAddPlace, isLoading }) {
           },
           pattern: {
             value:
-              /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+              /(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/,
             message: "Invalid URL format",
           },
         })}
--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -43,7 +43,7 @@ function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar, isLoading }) {
           },
           pattern: {
             value:
-              /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+              /(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/,
             message: "Invalid URL format",
           },
         })}
